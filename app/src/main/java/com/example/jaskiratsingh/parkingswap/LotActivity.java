4dbee7d9714fc9196aca13a7e5004f53c4547762
jsingh16 <jsingh27@buffalo.edu>
2016-10-24 14:36:22 -0400

    Linked south Campus button to a map

--- app/src/main/java/com/example/jaskiratsingh/parkingswap/LotActivity.java
+++ app/src/main/java/com/example/jaskiratsingh/parkingswap/LotActivity.java
@@ -1,21 +1,30 @@
 package com.example.jaskiratsingh.parkingswap;
 
+import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TabHost;
 
 /**
  * Created by jaskiratsingh on 9/14/16.
  */
-public class LotActivity extends AppCompatActivity {
+
+
+public class LotActivity extends AppCompatActivity{
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lots);
+
         TabHost tabHost =(TabHost)findViewById(R.id.tabHost);
         tabHost.setup();
 
@@ -32,95 +41,38 @@ public class LotActivity extends AppCompatActivity {
     }
 
 
-    public void onButtonClickArena(View v){
+    public void onButtonClickSouth(View v){
+        switch (v.getId()){
+            case R.id.Abbott_A:
+                //((findButton)this.getApplication()).setID("Abbot_A");
+                startActivity(new Intent("Campus_class.Campus"));
+                break;
+            case R.id.Clark:
+               // ((findButton)this.getApplication()).setID("Clark");
+                startActivity(new Intent("Campus_class.Campus"));
+                break;
+            case R.id.Main_Bailey:
+                //((findButton)this.getApplication()).setID("Main_Bailey");
+                startActivity(new Intent("Campus_class.Campus"));
+                break;
+            case R.id.Parker:
+                //((findButton)this.getApplication()).setID("Parker");
+                startActivity(new Intent("Campus_class.Campus"));
+                break;
+            case R.id.Sherman:
+                //((findButton)this.getApplication()).setID("Sherman");
+                startActivity(new Intent("Campus_class.Campus"));
+                break;
+            case R.id.Townsend:
+                //((findButton)this.getApplication()).setID("Townsend");
+                startActivity(new Intent("Campus_class.Campus"));
+                break;
 
-        startActivity(new Intent("Arena_class.Arena"));
-    }
-/*
-    public void onButtonClickButton2(View v){
-        startActivity(new Intent("Alumni_class.Map"));
 
-    }
 
-    public void onButtonClickButton3(View v){
-        startActivity(new Intent("Baird_class.Map"));
+        }
 
-    }
 
-    public void onButtonClickButton4(View v){
-        startActivity(new Intent("Cooke_class.Map"));
-
-    }
-
-    public void onButtonClickButton5(View v){
-        startActivity(new Intent("Crofts_class.Map"));
-
-    }
-
-    public void onButtonClickButton6(View v){
-        startActivity(new Intent("Fargo_class.Map"));
-
-    }
-
-    public void onButtonClickButton7(View v){
-        startActivity(new Intent("Furnas_class.Map"));
-
-    }
-
-    public void onButtonClickButton8(View v){
-        startActivity(new Intent("Governors_class.Map"));
 
     }
-
-    public void onButtonClickButton9(View v){
-        startActivity(new Intent("Hochstetter_class.Map"));
-
-    }
-
-    public void onButtonClickButton10(View v){
-        startActivity(new Intent("Jacobs_class.Map"));
-
-    }
-
-    public void onButtonClickButton11(View v){
-        startActivity(new Intent("Jarvis_class.Map"));
-
-    }
-
-    public void onButtonClickButton12(View v){
-        startActivity(new Intent("Ketter_class.Map"));
-
-    }
-
-    public void onButtonClickButton13(View v){
-        startActivity(new Intent("LakeLaSalle_class.Map"));
-
-    }
-
-    public void onButtonClickButton14(View v){
-        startActivity(new Intent("RedJacket_class.Map"));
-
-    }
-
-    public void onButtonClickButton15(View v){
-        startActivity(new Intent("Richmond_class.Map"));
-
-    }
-
-    public void onButtonClickButton16(View v){
-        startActivity(new Intent("Stadium_class.Map"));
-
-    }
-
-    public void onButtonClickButton17(View v){
-        startActivity(new Intent("Slee_class.Map"));
-
-    }
-
-    public void onButtonClickButton18(View v){
-        startActivity(new Intent("Spaulding_class.Map"));
-
-    }
-*/
-
 }
