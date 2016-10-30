4fd959db8cf6c9247ac6695009c0c438d76bbcd4
tjquinn0218 <tjquinn@buffalo.ed>
2016-10-29 20:45:16 -0400

    Parking lot queues class

---
+++ parkingQs.java
@@ -0,0 +1,107 @@
+import java.util.*;
+
+public class Queues {
+	public static void main(String[] args){
+		if(id.equals("Arena")){
+			// server coming soon(:
+			Queue<String> Arena = new LinkedList<String>();
+		}else if(id.equals("Alumni A")){
+			// server coming soon
+			Queue<String> AlumniA = new LinkedList<String>();
+		}else if(id.equals("Alumni B")){
+			// server coming soon
+			Queue<String> AlumniB = new LinkedList<String>();
+		}else if(id.equals("Alumni C")){
+			// server coming soon
+			Queue<String> AlumniC = new LinkedList<String>();
+		}else if(id.equals("Baird A")){
+			// server coming soon
+			Queue<String> BairdA= new LinkedList<String>();
+		}else if(id.equals("Cook A")){
+			// server coming soon
+			Queue<String> CookA = new LinkedList<String>();
+		}else if(id.equals("Cook B")){
+			// server coming soon
+			Queue<String> CookB = new LinkedList<String>();
+		}else if(id.equals("Fargo")){
+			// server coming soon
+			Queue<String> Fargo = new LinkedList<String>();
+		}else if(id.equals("Governors B")){
+			// server coming soon
+			Queue<String> GovernorsB = new LinkedList<String>();
+		}else if(id.equals("Governors C")){
+			// server coming soon
+			Queue<String> GovernorsC = new LinkedList<String>();
+		}else if(id.equals("Governors D")){
+			// server coming soon
+			Queue<String> GovernorsD = new LinkedList<String>();
+		}else if(id.equals("Governors E")){
+			// server coming soon
+			Queue<String> GovernorsE = new LinkedList<String>();
+		}else if(id.equals("Hochstetter B (Commuter Only Lot)")){
+			// server coming soon
+			Queue<String> HochstetterB = new LinkedList<String>();
+		}
+		else if(id.equals("Jacobs B")){
+			// server coming soon
+			Queue<String> JacobsB = new LinkedList<String>();
+		}
+		else if(id.equals("Jacobs C")){
+			// server coming soon
+			Queue<String> JacobsC = new LinkedList<String>();
+		}else if(id.equals("Jarvis A (Commuter Only Lot)")){
+			// server coming soon
+			Queue<String> JarvisA = new LinkedList<String>();
+		}else if(id.equals("Jarvis B")){
+			// server coming soon
+			Queue<String> JarvisB = new LinkedList<String>();
+		}else if(id.equals("Ketter")){
+			// server coming soon
+			Queue<String> Ketter = new LinkedList<String>();
+		}else if(id.equals("Lake LaSalle")){
+			// server coming soon
+			Queue<String> Lake_LaSalle = new LinkedList<String>();
+		}else if(id.equals("Red Jacket")){
+			// server coming soon
+			Queue<String> Red_Jacket = new LinkedList<String>();
+		}else if(id.equals("Richmond A")){
+			// server coming soon
+			Queue<String> RichmondA = new LinkedList<String>();
+		}else if(id.equals("Richmond B")){
+			// server coming soon
+			Queue<String> RichmondB = new LinkedList<String>();
+		}else if(id.equals("Special Event Parking")){
+			// server coming soon
+			Queue<String> Special = new LinkedList<String>();
+		}else if(id.equals("Stadium")){
+			// server coming soon
+			Queue<String> Stadium = new LinkedList<String>();
+		}else if(id.equals("Slee A")){
+			// server coming soon
+			Queue<String> SleeA = new LinkedList<String>();
+		}else if(id.equals("Slee B")){
+			// server coming soon
+			Queue<String> SleeB = new LinkedList<String>();
+		}else if(id.equals("Spaulding (One Row of Spaulding Faculty/Staff Only)")){
+			// server coming soon
+			Queue<String> Spaulding = new LinkedList<String>();
+		}else if(id.equals("Abbott A")){
+			// server coming soon
+			Queue<String> AbbottA = new LinkedList<String>();
+		}else if(id.equals("Clark (One Row Signed for Faculty/Staff Only)")){
+			// server coming soon
+			Queue<String> Clark = new LinkedList<String>();
+		}
+		else if(id.equals("Main-Bailey")){
+			// server coming soon
+			Queue<String> Main_Bailey = new LinkedList<String>();
+		}else if(id.equals("Parker")){
+			// server coming soon
+			Queue<String> Parker = new LinkedList<String>();
+		}else if(id.equals("Sherman")){
+			// server coming soon
+			Queue<String> Sherman = new LinkedList<String>();
+		}else // townsend is left
+		{ // server coming soon
+			Queue<String> Arena = new LinkedList<String>();
+		}
