287478237892890f5a4a5670958aa622bafe6cfd
jsingh16 <jsingh27@buffalo.edu>
2016-10-30 14:45:29 -0400

    Merge develop into master

---
+++ app/src/main/java/com/parkingswap/GetClient.java
@@ -0,0 +1,37 @@
+package com.parkingswap;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class GetClient extends Activity {
+
+    TextView response;
+    Button buttonConnect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_server);
+
+        buttonConnect = (Button) findViewById(R.id.connectButton);
+        response = (TextView) findViewById(R.id.T1);
+
+        buttonConnect.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Server myClient = new Server(response);
+                myClient.execute();
+                response.setText(Server.who);
+            }
+
+        });
+
+
+    }
+
+}
--- javaServer/ParkerBot.java
+++ javaServer/ParkerBot.java
@@ -4,14 +4,17 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ParkerBot implements Runnable
 {
    private Socket s;
    private Scanner in;
    private PrintWriter out;
-   private Queue parkers;
-   private Queue leavers;
+   public ArrayList<String> parkingList = new ArrayList<String>();
+   public static Queue<String> leavers = new LinkedList();
+   public static Queue<String> parkers = new LinkedList();
+   public ReentrantLock lock = new ReentrantLock();
 
    public ParkerBot(Socket socke)
    {
@@ -55,17 +58,76 @@ public class ParkerBot implements Runnable
 			{
 				return;
 			}
-			String message = in.nextLine();
+			String message = in.next();
+			//String test = message.replace("\\r","");
+			String test = message;
+			System.out.println("TEST VAL:");
+			System.out.println(test);
+			if(test.equals("PARK"))
+			{
+				String id = in.next();
+				System.out.println(id);
+				lock.lock();
+				parkers.add(id);
+				lock.unlock();
+				out.println("OOO yeah FINDING YOU A SPOT \n");
+				out.flush();
+				lock.lock();
+				String M = leavers.poll();
+				lock.unlock();
+				while(M ==null )
+				{
+					lock.lock();
+					M = leavers.poll();
+					lock.unlock();
+				}
+				//System.out.println(M);
+				out.println(M);
+				out.flush();
 
-			System.out.println(message);
+				continue;
+			}
+			else if(test.equals("LEAVE"))
+			{
+				String id = in.next();
+				System.out.println(id);
+				lock.lock();
+				leavers.add(id);
+				lock.unlock();
+				out.println("GETTING YOU HOOKED UP WITH A PARKER \n");
+				out.flush();
+				lock.lock();
+				System.out.println(parkers);
+				String M = parkers.poll();
+				lock.unlock();
+				while(M == null)
+				{
+					    lock.lock();
+						M = parkers.poll();
+						lock.unlock();
+				}
+								//System.out.println(M);
+				out.println(M);
+
+				//out.println("mark");
+				out.flush();
+
+
+
+				continue;
+			}
+
+			else
+			{
+				System.out.println(test.length());
+				out.println("PLEASE ENTER EITHER PARK OR LEAVE");
+				out.flush();
+				continue;
+
+			}
+
 
 		}
-	}
-
-	public void runParker(String command)
-	{
-
-
 	}
 
 }
